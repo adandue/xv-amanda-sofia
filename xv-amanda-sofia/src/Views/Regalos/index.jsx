6b6aab9573c0a72300378fbdfce3d698740378f3
adan_due <ing.adanduenas@gmail.com>
2024-08-29 12:46:25 -0600

    Cambiar el orden de los regalos

--- xv-amanda-sofia/src/Views/Regalos/index.jsx
+++ xv-amanda-sofia/src/Views/Regalos/index.jsx
@@ -13,14 +13,15 @@ const Regalos = () => {
                         <h4 className='title ' style={{fontFamily: 'EdwardianScript'}}>Si gustan también puede ser</h4>
                     </div>
                     <div className='evento'>
+                        <FaEnvelope  className="icon"/>
+                        <p className='text' style={{fontFamily: 'Baskerville'}}>Lluvia de sobres</p>
+                        <p className='text' style={{fontFamily: 'Baskerville'}}>Si gusta dar su regalo con dinero</p>
+                    </div>
+                    <div className='evento'>
                         <GiPresent className="icon"/>
                         <p className='text' style={{fontFamily: 'Baskerville'}}>Liverpool</p>
                         <p className='text' style={{fontFamily: 'Baskerville'}}>No. de evento</p>
                         <p className='text' style={{fontFamily: 'Baskerville'}}>51 48 64 33</p>
-                    </div>
-                    <div className='evento'>
-                        <FaEnvelope  className="icon"/>
-                        <p className='text' style={{fontFamily: 'Baskerville'}}>Lluvia de sobres</p>
                     </div>
                 </div>
             </div>
