0381b9981b099de84c87ca5774f548c4173747cc
adan_due <ing.adanduenas@gmail.com>
2024-08-30 11:45:18 -0600

    Añadiendo el marco a la primera pantalla

--- xv-amanda-sofia/src/Views/Hero/index.jsx
+++ xv-amanda-sofia/src/Views/Hero/index.jsx
@@ -1,3 +1,4 @@
+import frame from '../../assets/frame.svg'
 import escudo from '../../assets/escudo.svg'
 import './hero.css'
 
@@ -5,8 +6,9 @@ const Hero = () => {
     return (
         <>
         <div className='wrapper'>
-            <div className='background'>
-                <img src={escudo} alt='Logo' className='Logo'/>
+            <div className='background' id='backgroundHero'>
+                <img src={frame} alt='marco' className='marco' />
+                <img src={escudo} alt='Logo' className='logo'/>
             </div>
         </div>
         </>
