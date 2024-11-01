237f96074d6974454a32db5441ad8de5a4deed7a
adan_due <ing.adanduenas@gmail.com>
2024-11-01 13:21:54 -0600

    Se cambió la sección de confirmar a 4 filas y se puso el texto de evitar el color azul en strong, se checa responsive

--- xv-amanda-sofia/src/Views/Confirmar/index.jsx
+++ xv-amanda-sofia/src/Views/Confirmar/index.jsx
@@ -6,16 +6,19 @@ const Confirmar = () => {
     return (
         <>
         <div className='wrapper'>
-            <div className='background' id='backgrounConfirmar'>
+            <div className='background' id='backgroundConfirmar'>
                 <h1 className='title' id='confirmarTitle' style={{fontFamily: 'EdwardianScript'}}>Confirma tu asistencia</h1>
                 <div className='evento'>
                     <p className='text'>Será una noche increíble</p>
                     <p className='text'>¡Lo esperamos!</p>
-                    <p className="dress-code">Agradecemos su puntualidad</p>
-                    <p className="dress-code">Dress code: Etiqueta rigurosa</p>
-                    <p className="dress-code">En atención al protocolo, rogamos evitar el color azul</p>
                 </div>
-                    <a className="button" href={linkWhatsApp} rel="follow noopener" role="button" target="_blank">
+                <div className="dress-code" id="dress-code">
+                    <p className="dress-code-text">ATENCIÓN:</p>
+                    <p className="dress-code-text">Dress code: Etiqueta rigurosa</p>
+                    <p className="dress-code-text">Agradecemos su puntualidad</p>
+                    <p className="dress-code-text"><strong>En atención al protocolo, rogamos evitar el color azul</strong></p>
+                </div>
+                    <a className="button" id="confirmar-button" href={linkWhatsApp} rel="follow noopener" role="button" target="_blank">
                         <div>
                             <FaWhatsapp />
                             <span >
